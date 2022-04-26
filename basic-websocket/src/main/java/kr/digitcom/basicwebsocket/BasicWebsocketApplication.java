adf0b8c6a45d5bba208d0b2376ac6c6d91290c66
y-hoon <digitcomgisul@gmail.com>
2022-04-26 09:04:06 +0900

    웹소켓 기본예제 프로젝트 init

---
+++ basic-websocket/src/main/java/kr/digitcom/basicwebsocket/BasicWebsocketApplication.java
@@ -0,0 +1,13 @@
+package kr.digitcom.basicwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicWebsocketApplication.class, args);
+    }
+
+}
---
+++ basic-websocket/src/test/java/kr/digitcom/basicwebsocket/BasicWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.digitcom.basicwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
